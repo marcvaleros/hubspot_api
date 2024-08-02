24e5da01abd0869fed2e27866e5aa8bb2134325d
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-08-02 23:46:11 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import {tcase} from './hubspot.mjs';
 
 dotenv.config()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = new Slack.App({
   token: process.env.SLACK_BOT_TOKEN,
