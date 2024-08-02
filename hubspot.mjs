d817916f196de1f466b62868a17abd9b25bfb164
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-08-02 18:01:36 +0800

    finished

---
+++ hubspot.mjs
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const token = process.env.HUBSPOT_API_KEY;
+const baseURL = 'https://api.hubapi.com/';
+const endpoint = 'crm/v3/objects/companies?limit=100&properties=people,name,phone,email,mailing_address,dot_number,mc_number,ff.number,interstate_drivers,cdl_drivers,ff_number';
+let companies;
+
+//get all the companies, filter and return the latest company
+export const getCompanies = async () => {
+    const response = await axios.get(`${baseURL}${endpoint}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      companies = response.data.results.reduce((latest, company) => {
+        return new Date(latest.properties.createdate) > new Date(company.properties.createdate) ? latest : company;
+      });
+    }).catch(error => {
+      console.error('Error fetching data:', error);
+    });
+    
+    return companies;
+}
+
+export const convertDate = (isoDate) => {
+  const date = new Date(isoDate);
+  const readableDate = date.toLocaleString();
+
+  return readableDate;
+}
+
+export const tcase =(str) => {
+    return str.toLowerCase().split(' ').map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+}
+
